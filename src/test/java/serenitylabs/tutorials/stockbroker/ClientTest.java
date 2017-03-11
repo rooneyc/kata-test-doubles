552e5a8e48ce30020229b415e04291a762a871a6
rooneyc <conor.p.rooney@gmail.com>
2017-03-11 21:57:19 +0000

    refactor

--- src/test/java/serenitylabs/tutorials/stockbroker/ClientTest.java
+++ src/test/java/serenitylabs/tutorials/stockbroker/ClientTest.java
@@ -10,41 +10,53 @@ public class ClientTest {
     @Test
     public void should_be_able_to_place_an_empty_order() throws Exception {
 
-        //When
+        //Given
         Client client = new Client(null, null);
 
+        //When
+        String orderSummary = client.place("");
+
         //Then
-        assertThat(client.place("")).isEqualTo("Buy: USD 0.00, Sell: USD 0.00");
+        assertThat(orderSummary).isEqualTo("Buy: USD 0.00, Sell: USD 0.00");
     }
 
     @Test
     public void should_be_able_to_place_a_single_buy_order() throws Exception {
 
-        //When
+        //Given
         Client client = new Client(null, null);
 
+        //When
+        String orderSummary = client.place("GOOG 300 829.08 B");
+
         //Then
-        assertThat(client.place("GOOG 300 829.08 B")).isEqualTo("Buy: USD 248724.00, Sell: USD 0.00");
+        assertThat(orderSummary).isEqualTo("Buy: USD 248724.00, Sell: USD 0.00");
     }
 
     @Test
     public void should_be_able_to_place_a_single_sell_order() throws Exception {
 
-        //When
+        //Given
         Client client = new Client(null, null);
 
+        //When
+        String orderSummary = client.place("FB 320 137.17 S");
+
         //Then
-        assertThat(client.place("GOOG 300 829.08 S")).isEqualTo("Buy: USD 0.00, Sell: USD 248724.00");
+        assertThat(orderSummary).isEqualTo("Buy: USD 0.00, Sell: USD 43894.40");
     }
 
     @Test
-    public void should_be_able_to_place_a_multi_order() throws Exception {
+    public void should_be_able_to_place_a_double_order() throws Exception {
 
-        //When
+        //Given
         Client client = new Client(null, null);
 
+        //When
+        String orderSummary = client.place("GOOG 300 829.08 B,FB 320 137.17 S");
+
         //Then
-        assertThat(client.place("ZNGA 1300 2.78 B,AAPL 50 139.78 B,FB 320 137.17 S")).isEqualTo("Buy: USD 10603.00, Sell: USD 43894.40");
+        assertThat(orderSummary).isEqualTo("Buy: USD 248724.00, Sell: USD 43894.4");
     }
 
 }
