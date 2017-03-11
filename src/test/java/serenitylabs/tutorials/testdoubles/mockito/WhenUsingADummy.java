c43451830705c8d98703e41e8a0ad9146943fcfa
rooneyc <conor.p.rooney@gmail.com>
2017-03-11 18:06:48 +0000

    we_make_it_return_a_null_so_that_it_does_not_get_used_accidentally

--- src/test/java/serenitylabs/tutorials/testdoubles/mockito/WhenUsingADummy.java
+++ src/test/java/serenitylabs/tutorials/testdoubles/mockito/WhenUsingADummy.java
@@ -1,11 +1,16 @@
 package serenitylabs.tutorials.testdoubles.mockito;
 
+import jdk.internal.org.objectweb.asm.tree.TryCatchBlockNode;
 import org.junit.Test;
 import serenitylabs.tutorials.testdoubles.Authoriser;
 import serenitylabs.tutorials.testdoubles.System;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class WhenUsingADummy {
 
@@ -25,11 +30,30 @@ public class WhenUsingADummy {
 
     @Test
     public void we_make_it_return_a_null_so_that_it_does_not_get_used_accidentally() {
+
         /*
          * Mockito always returns a 'nice' value, so we son't get a null unless the method in question
          * returns a custom object
          *
          * https://github.com/mockito/mockito/wiki/FAQ#what-values-do-mocks-return-by-default
          */
+
+        //So we don't need this this when mocking as there is no dummyAuthoriser that could be used accidentally
+        //But for practise we can stub the authorise method so that it returns a null.
+
+        //Given
+        Authoriser authoriser = mock(Authoriser.class);
+        System system = new System(authoriser);
+
+        when(authoriser.authorise(anyString(), anyString())).thenReturn(null);
+
+        try {
+            //When
+            system.login("bob", "SecretPassword");
+            failBecauseExceptionWasNotThrown(IndexOutOfBoundsException.class);
+        } catch (NullPointerException e){
+            //Then
+            assertThat(e).isInstanceOf(NullPointerException.class);
+        }
     }
 }
