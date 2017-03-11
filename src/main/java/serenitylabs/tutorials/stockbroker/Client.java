88be523a2af4e9be31d301d4bee4e788e60c5d89
rooneyc <conor.p.rooney@gmail.com>
2017-03-11 21:11:29 +0000

    refactor - ok that broke the first test......

--- src/main/java/serenitylabs/tutorials/stockbroker/Client.java
+++ src/main/java/serenitylabs/tutorials/stockbroker/Client.java
@@ -2,6 +2,9 @@ package serenitylabs.tutorials.stockbroker;
 
 import serenitylabs.tutorials.stockbroker.parser.OrderParser;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Client {
     private final StockBroker broker;
     private final OrderParser parser;
@@ -13,8 +16,15 @@ public class Client {
 
     public String place(String orders) {
 
+        // looks like we're missing something here ;-)
         double bought = 0.00;
         double sold = 0.00;
+
+        List<String> orderParts = Arrays.asList(orders.split("\\s"));
+
+        if ("B".equals(orderParts.get(3))) {
+            bought = Integer.parseInt(orderParts.get(1)) * Double.parseDouble(orderParts.get(2));
+        }
 
         return "Buy: USD " + String.format("%.2f", bought) +", Sell: USD " + String.format("%.2f", sold);
     }
