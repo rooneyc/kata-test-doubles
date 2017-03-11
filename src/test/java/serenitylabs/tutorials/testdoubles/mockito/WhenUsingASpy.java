0984278157e53218789fc271f832ee2cc97d3d74
rooneyc <conor.p.rooney@gmail.com>
2017-03-11 19:22:12 +0000

    we_want_to_ensure_that_the_spy_was_called

--- src/test/java/serenitylabs/tutorials/testdoubles/mockito/WhenUsingASpy.java
+++ src/test/java/serenitylabs/tutorials/testdoubles/mockito/WhenUsingASpy.java
@@ -2,12 +2,28 @@ package serenitylabs.tutorials.testdoubles.mockito;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import serenitylabs.tutorials.testdoubles.Authoriser;
+import serenitylabs.tutorials.testdoubles.System;
+
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class WhenUsingASpy {
 
     @Test
-    @Ignore("Implement me, please ...")
     public void we_want_to_ensure_that_the_spy_was_called() {
+
+        //Given
+        Authoriser authoriser = mock(Authoriser.class);
+        System system = new System(authoriser);
+
+        //When
+        system.login("bob", "SecretPassword");
+
+        ///Then
+        verify(authoriser).authorise(anyString(), anyString());
 
     }
 }
