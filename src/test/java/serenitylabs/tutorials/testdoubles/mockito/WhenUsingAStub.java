a0a855cff4a4a27497df8021c69f9c8b0bc15a01
rooneyc <conor.p.rooney@gmail.com>
2017-03-11 19:07:42 +0000

    we_want_to_control_the_returned_value_to_make_the_system_under_test_accept_the_login_attempt

--- src/test/java/serenitylabs/tutorials/testdoubles/mockito/WhenUsingAStub.java
+++ src/test/java/serenitylabs/tutorials/testdoubles/mockito/WhenUsingAStub.java
@@ -2,12 +2,30 @@ package serenitylabs.tutorials.testdoubles.mockito;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import serenitylabs.tutorials.testdoubles.Authoriser;
+import serenitylabs.tutorials.testdoubles.System;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 public class WhenUsingAStub {
 
     @Test
-    @Ignore("Implement me, please ...")
     public void we_want_to_control_the_returned_value_to_make_the_system_under_test_accept_the_login_attempt() {
+
+        //Given
+        Authoriser authoriser = mock(Authoriser.class);
+        System system = new System(authoriser);
+
+        given(authoriser.authorise(anyString(), anyString())).willReturn(true);
+
+        //When
+        system.login("bob", "SecretPassword");
+
+        //Then
+        assertThat(system.numberOfActiveUsers()).isEqualTo(1);
 
     }
 
