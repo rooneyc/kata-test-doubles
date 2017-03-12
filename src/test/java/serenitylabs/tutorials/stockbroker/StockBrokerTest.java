dc6694eaf4c3014fd9eccd9d7b09e0c183e8f0b8
rooneyc <conor.p.rooney@gmail.com>
2017-03-12 16:13:59 +0000

    Refactor StockBrokerTest

--- src/test/java/serenitylabs/tutorials/stockbroker/StockBrokerTest.java
+++ src/test/java/serenitylabs/tutorials/stockbroker/StockBrokerTest.java
@@ -7,6 +7,8 @@ import serenitylabs.tutorials.stockbroker.exchange.OrderType;
 import serenitylabs.tutorials.stockbroker.exchange.StockExchange;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -37,9 +39,9 @@ public class StockBrokerTest {
         StockExchange exchange = mock(StockExchange.class);
         StockBroker broker = new StockBroker(exchange);
         //And
-        Order order = new Order("GOOG", 300, Money.parse("USD 829.08"), OrderType.Buy);
-        List<Order> orders = new ArrayList<>();
-        orders.add(order);
+        List<Order> orders = Collections.singletonList(
+                new Order("GOOG", 300, Money.parse("USD 829.08"), OrderType.Buy)
+        );
 
         //When
         OrderSummaryInterface orderSummary = broker.place(orders);
@@ -55,9 +57,9 @@ public class StockBrokerTest {
         StockExchange exchange = mock(StockExchange.class);
         StockBroker broker = new StockBroker(exchange);
         //And
-        Order order = new Order("FB", 320, Money.parse("USD 137.17"), OrderType.Sell);
-        List<Order> orders = new ArrayList<>();
-        orders.add(order);
+        List<Order> orders = Collections.singletonList(
+                new Order("FB", 320, Money.parse("USD 137.17"), OrderType.Sell)
+        );
 
         //When
         OrderSummaryInterface orderSummary = broker.place(orders);
@@ -73,11 +75,10 @@ public class StockBrokerTest {
         StockExchange exchange = mock(StockExchange.class);
         StockBroker broker = new StockBroker(exchange);
         //And
-        Order firstOrder = new Order("GOOG", 300, Money.parse("USD 829.08"), OrderType.Buy);
-        Order secondOrder = new Order("FB", 320, Money.parse("USD 137.17"), OrderType.Sell);
-        List<Order> orders = new ArrayList<>();
-        orders.add(firstOrder);
-        orders.add(secondOrder);
+        List<Order> orders = Arrays.asList(
+            new Order("GOOG", 300, Money.parse("USD 829.08"), OrderType.Buy),
+            new Order("FB", 320, Money.parse("USD 137.17"), OrderType.Sell)
+        );
 
         //When
         OrderSummaryInterface orderSummary = broker.place(orders);
@@ -95,13 +96,11 @@ public class StockBrokerTest {
         StockExchange exchange = mock(StockExchange.class);
         StockBroker broker = new StockBroker(exchange);
         //And
-        Order firstOrder = new Order("ZNGA", 1300, Money.parse("USD 2.78"), OrderType.Buy);
-        Order secondOrder = new Order("AAPL", 50, Money.parse("USD 139.78"), OrderType.Buy);
-        Order thirdOrder = new Order("FB", 320, Money.parse("USD 137.17"), OrderType.Sell);
-        List<Order> orders = new ArrayList<>();
-        orders.add(firstOrder);
-        orders.add(secondOrder);
-        orders.add(thirdOrder);
+        List<Order> orders = Arrays.asList(
+                new Order("ZNGA", 1300, Money.parse("USD 2.78"), OrderType.Buy),
+                new Order("AAPL", 50, Money.parse("USD 139.78"), OrderType.Buy),
+                new Order("FB", 320, Money.parse("USD 137.17"), OrderType.Sell)
+        );
 
         //When
         OrderSummaryInterface orderSummary = broker.place(orders);
