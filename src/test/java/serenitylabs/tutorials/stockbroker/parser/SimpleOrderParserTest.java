e51d3ebe399c167d482e413b136b7b0dd3222e16
rooneyc <conor.p.rooney@gmail.com>
2017-03-12 10:47:57 +0000

    should_be_able_to_parse_an_empty_order_string failing

--- src/test/java/serenitylabs/tutorials/stockbroker/parser/SimpleOrderParserTest.java
+++ src/test/java/serenitylabs/tutorials/stockbroker/parser/SimpleOrderParserTest.java
@@ -12,18 +12,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SimpleOrderParserTest {
 
     @Test
-    public void should_be_able_to_parse_an_order_string() throws Exception {
+    public void should_be_able_to_parse_an_empty_order_string() {
 
         //Given
-        String orderString = "GOOG 300 829.08 B";
         SimpleOrderParser orderParser = new SimpleOrderParser();
-        Order parsedOrder = new Order("GOOG", 300, Money.parse("USD 829.08"), OrderType.Buy);
 
         //When
-        List<Order> orders = orderParser.parse("GOOG 300 829.08 B");
+        List<Order> orders = orderParser.parse("");
 
         //Then
-        assertThat(orders).contains(parsedOrder);
+        assertThat(orders).isEmpty();
     }
+
+
+//    @Test
+//    public void should_be_able_to_parse_an_order_string() throws Exception {
+//
+//        //Given
+//        String orderString = "GOOG 300 829.08 B";
+//        SimpleOrderParser orderParser = new SimpleOrderParser();
+//        Order parsedOrder = new Order("GOOG", 300, Money.parse("USD 829.08"), OrderType.Buy);
+//
+//        //When
+//        List<Order> orders = orderParser.parse("GOOG 300 829.08 B");
+//
+//        //Then
+//        assertThat(orders).contains(parsedOrder);
+//    }
 
 }
