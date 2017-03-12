a05e88dda32682552af8417c45f5798a3c3e7441
rooneyc <conor.p.rooney@gmail.com>
2017-03-12 13:37:14 +0000

    should_be_able_to_place_an_empty_order passing

--- src/main/java/serenitylabs/tutorials/stockbroker/StockBroker.java
+++ src/main/java/serenitylabs/tutorials/stockbroker/StockBroker.java
@@ -1,5 +1,6 @@
 package serenitylabs.tutorials.stockbroker;
 
+import org.joda.money.Money;
 import serenitylabs.tutorials.stockbroker.exchange.Order;
 import serenitylabs.tutorials.stockbroker.exchange.StockExchange;
 
@@ -13,6 +14,23 @@ public class StockBroker {
     }
 
     public OrderSummary place(List<Order> orders) {
-        throw new RuntimeException("Implement me, please...");
+
+        return new OrderSummary(){
+
+            @Override
+            public Money buyTotal() {
+                return Money.parse("USD 0.00");
+            }
+
+            @Override
+            public Money sellTotal() {
+                return Money.parse("USD 0.00");
+            }
+
+            @Override
+            public List<Order> failedOrders() {
+                return null;
+            }
+        };
     }
 }
