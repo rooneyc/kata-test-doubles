1899c466e00aebdffaee0bb8ec205dfea1e533e0
rooneyc <conor.p.rooney@gmail.com>
2017-03-12 10:48:25 +0000

    should_be_able_to_parse_an_empty_order_string passing

--- src/main/java/serenitylabs/tutorials/stockbroker/parser/SimpleOrderParser.java
+++ src/main/java/serenitylabs/tutorials/stockbroker/parser/SimpleOrderParser.java
@@ -2,12 +2,13 @@ package serenitylabs.tutorials.stockbroker.parser;
 
 import serenitylabs.tutorials.stockbroker.exchange.Order;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleOrderParser implements OrderParser {
 
     @Override
     public List<Order> parse(String orders) {
-        return null;
+        return new ArrayList<>();
     }
 }
