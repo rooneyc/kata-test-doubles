48d335e7ed81f669765df56cfa0d2c3ba5e20c94
rooneyc <conor.p.rooney@gmail.com>
2017-03-18 15:57:51 +0000

    WhenUsingADummy we_make_it_return_a_null_so_that_it_does_not_get_used_accidentally

--- src/test/java/serenitylabs/tutorials/testdoubles/mockito/WhenUsingADummy.java
+++ src/test/java/serenitylabs/tutorials/testdoubles/mockito/WhenUsingADummy.java
@@ -5,6 +5,10 @@ import serenitylabs.tutorials.testdoubles.Authoriser;
 import serenitylabs.tutorials.testdoubles.System;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 public class WhenUsingADummy {
@@ -13,10 +17,10 @@ public class WhenUsingADummy {
     public void we_do_not_worry_about_the_value_it_returns_because_we_know_it_will_never_be_used() {
 
         //Given
-        Authoriser dummyAuthoriser = mock(Authoriser.class);
+        Authoriser authoriser = mock(Authoriser.class);
 
         //When
-        System system = new System(dummyAuthoriser);
+        System system = new System(authoriser);
 
         //Then
         assertThat(system.numberOfActiveUsers()).isEqualTo(0);
@@ -25,11 +29,20 @@ public class WhenUsingADummy {
 
     @Test
     public void we_make_it_return_a_null_so_that_it_does_not_get_used_accidentally() {
-        /*
-         * Mockito always returns a 'nice' value, so we son't get a null unless the method in question
-         * returns a custom object
-         *
-         * https://github.com/mockito/mockito/wiki/FAQ#what-values-do-mocks-return-by-default
-         */
+
+        //Given
+        Authoriser authoriser = mock(Authoriser.class);
+        System system = new System(authoriser);
+        given(authoriser.authorise(anyString(), anyString())).willReturn(null);
+
+        try {
+            //When
+            system.login("bob", "SecretPassword");
+            failBecauseExceptionWasNotThrown(NullPointerException.class);
+        } catch (NullPointerException e) {
+            //Then
+            assertThat(e).isInstanceOf(NullPointerException.class);
+        }
+
     }
 }
