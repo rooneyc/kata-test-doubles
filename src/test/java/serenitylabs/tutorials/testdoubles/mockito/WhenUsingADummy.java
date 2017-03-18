6aefe9b3e52410ffae163b749ae65cd6f4875320
rooneyc <conor.p.rooney@gmail.com>
2017-03-18 16:03:20 +0000

    WhenUsingAStub we_want_to_control_the_returned_value_to_make_the_system_under_test_accept_the_login_attempt

--- src/test/java/serenitylabs/tutorials/testdoubles/mockito/WhenUsingADummy.java
+++ src/test/java/serenitylabs/tutorials/testdoubles/mockito/WhenUsingADummy.java
@@ -6,7 +6,6 @@ import serenitylabs.tutorials.testdoubles.System;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
