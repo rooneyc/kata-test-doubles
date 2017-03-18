e437152ccf17719f1a79fe83790951c05fe6e1a3
rooneyc <conor.p.rooney@gmail.com>
2017-03-18 22:31:33 +0000

    WhenUsingAMock we_want_to_move_the_test_logic_from_the_test_to_the_mock_object

--- src/test/java/serenitylabs/tutorials/testdoubles/mockito/WhenUsingAMock.java
+++ src/test/java/serenitylabs/tutorials/testdoubles/mockito/WhenUsingAMock.java
@@ -2,12 +2,30 @@ package serenitylabs.tutorials.testdoubles.mockito;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import serenitylabs.tutorials.testdoubles.Authoriser;
+import serenitylabs.tutorials.testdoubles.System;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class WhenUsingAMock {
 
     @Test
-    @Ignore("Implement me, please ...")
     public void we_want_to_move_the_test_logic_from_the_test_to_the_mock_object() {
+
+        //Given
+        String username = "bob";
+        String password =  "SecretPassword";
+        Authoriser authoriser = mock(Authoriser.class);
+        System system = new System(authoriser);
+
+        //When
+        system.login(username, password);
+
+        //Then
+        verify(authoriser).authorise(username, password);
 
     }
 }
