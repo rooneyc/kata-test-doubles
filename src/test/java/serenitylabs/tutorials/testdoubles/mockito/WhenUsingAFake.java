08fdb5e09f889c2ca76c2b5c28176f2d31040e01
rooneyc <conor.p.rooney@gmail.com>
2017-03-18 22:35:55 +0000

    WhenUsingAFake we_want_to_control_the_behaviour_of_the_mock_depending_on_the_parameters

--- src/test/java/serenitylabs/tutorials/testdoubles/mockito/WhenUsingAFake.java
+++ src/test/java/serenitylabs/tutorials/testdoubles/mockito/WhenUsingAFake.java
@@ -1,13 +1,29 @@
 package serenitylabs.tutorials.testdoubles.mockito;
 
-import org.junit.Ignore;
 import org.junit.Test;
+import serenitylabs.tutorials.testdoubles.Authoriser;
+import serenitylabs.tutorials.testdoubles.System;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 public class WhenUsingAFake {
 
     @Test
-    @Ignore("Implement me, please ...")
     public void we_want_to_control_the_behaviour_of_the_mock_depending_on_the_parameters() {
+
+        //Given
+        Authoriser authoriser = mock(Authoriser.class);
+        given(authoriser.authorise("bob", "SecretPassword")).willReturn(Boolean.TRUE);
+        System system = new System(authoriser);
+
+        //When
+        system.login("bob", "SecretPassword");
+        system.login("kevin", "OtherSecretPassword");
+
+        //Then
+        assertThat(system.numberOfActiveUsers()).isEqualTo(1);
 
     }
 }
