11277ada662ccf3f8e98cc615e6dbfe017ae0580
rooneyc <conor.p.rooney@gmail.com>
2017-03-19 15:30:44 +0000

    refactor Client to use OrderSummary

--- src/main/java/serenitylabs/tutorials/stockbroker/Client.java
+++ src/main/java/serenitylabs/tutorials/stockbroker/Client.java
@@ -23,7 +23,7 @@ public class Client {
         List<Order> orderList = parser.parse(orders);
         OrderSummary summary = broker.place(orderList);
 
-        return "Buy: USD 248724.00, Sell: USD 0.00";
+        return "Buy: " + summary.buyTotal() + ", " + "Sell: " + summary.sellTotal();
 
     }
 }
