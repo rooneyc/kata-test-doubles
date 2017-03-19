5fdf63b9661dead506893a11212810d373092f26
rooneyc <conor.p.rooney@gmail.com>
2017-03-19 18:21:21 +0000

    ClientTest should_ask_stock_broker_to_place_order failing

--- src/test/java/serenitylabs/tutorials/stockbroker/ClientTest.java
+++ src/test/java/serenitylabs/tutorials/stockbroker/ClientTest.java
@@ -3,6 +3,7 @@ package serenitylabs.tutorials.stockbroker;
 import org.junit.Test;
 import serenitylabs.tutorials.stockbroker.parser.OrderParser;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -21,5 +22,21 @@ public class ClientTest {
 
         //Then
         verify(mockParser).parse(order);
+    }
+
+    @Test
+    public void should_ask_stock_broker_to_place_order() throws Exception {
+
+        //Given
+        String order = "GOOG 300 829.08 B";
+        OrderParser mockParser = mock(OrderParser.class);
+        StockBroker mockBroker = mock(StockBroker.class);
+        Client client = new Client(mockBroker, mockParser);
+
+        //When
+        client.place(order);
+
+        //Then
+        verify(mockBroker).place(any());
     }
 }
